8ea60a6cf1afa8db80355bcb0fead88189cee27b
huongbls <huongntFX16409@funix.edu.vn>
2022-05-19 00:02:33 +0700

    create StaffList component part 2

--- src/component/StaffListComponent.js
+++ src/component/StaffListComponent.js
@@ -16,18 +16,45 @@ class StaffList extends Component {
       selectStaff: null,
     };
   }
+  onStaffSelect(staff) {
+    this.setState({ selectStaff: staff });
+  }
+  renderStaff(staff) {
+    if (staff != null) {
+      return (
+        <Card>
+          <h4>Họ và tên: {staff.name}</h4>
+          <div>Ngày sinh: {staff.doB}</div>
+          <div>Ngày vào công ty: {staff.startDate}</div>
+          <div>Phòng ban: Hold</div>
+          <div>Số ngày nghỉ còn lại {staff.annualLeave}</div>
+          <div>Số ngày đã làm thêm: {staff.overTime}</div>
+        </Card>
+      );
+    } else {
+      return (
+        <div className="container">
+          <div className="row">
+            <div>Bấm vào tên nhân viên để xem thông tin</div>
+          </div>
+        </div>
+      );
+    }
+  }
   render() {
     const listStaff = this.props.staffs.map((staff) => {
       return (
         <div key={staff.id} className="col-12 col-md-4 m-1">
-          <Card>{staff.name}</Card>
+          <div onClick={() => this.onStaffSelect(staff)}>
+            <Card>{staff.name}</Card>
+          </div>
         </div>
       );
     });
     return (
       <div className="container">
         <div className="row">{listStaff}</div>
-        <div className="row">Bấm vào tên nhân viên để xem thông tin</div>
+        <div className="row">{this.renderStaff(this.state.selectStaff)}</div>
       </div>
     );
   }
