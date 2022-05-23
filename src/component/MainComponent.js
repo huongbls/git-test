817ea71c1d8b483ee3ae71af159e6aeec3ee61db
huongbls <huongntFX16409@funix.edu.vn>
2022-05-23 16:27:00 +0700

    Render Staffs

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -17,9 +17,53 @@ class Main extends Component {
   }
 
   render() {
+    // const StaffPage = () => {
+    //   return (
+    //     <StaffList
+    //       staffs={this.state.staffs}
+    //       promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+    //       leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+    //     />
+    //   );
+    // };
+
+    // const DishWithId = ({ match }) => {
+    //   return (
+    //     <DishDetail
+    //       dish={
+    //         this.state.dishes.filter(
+    //           (dish) => dish.id === parseInt(match.params.dishId, 10)
+    //         )[0]
+    //       }
+    //       comments={this.state.comments.filter(
+    //         (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+    //       )}
+    //     />
+    //   );
+    // };
+
     return (
       <div>
         <Header />
+        <Switch>
+          <Route
+            path="/staff"
+            component={() => <StaffList staffs={this.state.staffs} />}
+          />
+          {/* <Route
+            exact
+            path="/aboutus"
+            component={() => <About leaders={this.state.leaders} />}
+          />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} /> */}
+          <Redirect to="/staff" />
+        </Switch>
         <Footer />
       </div>
     );
--- src/component/StaffListComponent.js
+++ src/component/StaffListComponent.js
@@ -1,87 +1,33 @@
-import React, { Component } from "react";
-import dateFormat from "dateformat";
+import React from "react";
+import { Card, CardImg, CardTitle } from "reactstrap";
 
-class StaffList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectStaff: null,
-      selectColumn: 3,
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-    this.setState({ selectColumn: event.target.value });
-  }
-  onStaffSelect(staff) {
-    this.setState({ selectStaff: staff });
-  }
-  renderStaff(staff) {
-    if (staff != null) {
-      return (
-        <div className="col-12 col-md-12 col-lg-12 p-3">
-          <div className="p-3 text-danger border">
-            <h4>Họ và tên: {staff.name}</h4>
-            <div>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</div>
-            <div>
-              Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
-            </div>
-            <div>Phòng ban: {staff.department.name}</div>
-            <div>Số ngày nghỉ còn lại {staff.annualLeave}</div>
-            <div>Số ngày đã làm thêm: {staff.overTime}</div>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div className="container">
-          <div className="row">
-            <div className="pl-3">Bấm vào tên nhân viên để xem thông tin</div>
-          </div>
-        </div>
-      );
-    }
-  }
-  render() {
-    const col = this.state.selectColumn;
-    const x = "col-12 col-md-6 col-lg-" + 12 / col;
-    const listStaff = this.props.staffs.map((staff) => {
-      return (
-        <div key={staff.id} className={x}>
-          <div onClick={() => this.onStaffSelect(staff)}>
-            <div className="p-3 my-3 bg-primary text-white">{staff.name}</div>
-          </div>
-        </div>
-      );
-    });
+function RenderStaff({ staff }) {
+  return (
+    <div>
+      <Card>
+        <CardImg width="100%" src={staff.image} alt={staff.name} />
+        <CardTitle className="text-center">{staff.name}</CardTitle>
+      </Card>
+    </div>
+  );
+}
+
+function StaffList(props) {
+  const staffs = props.staffs.map((staff) => {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="d-none d-lg-block">
-            <label htmlFor="input-column" className="text-primary pl-3 pt-3">
-              Select display column (for laptop only)
-            </label>
-            <br></br>
-            <div className="col-lg-9 pb-3">
-              <select
-                className="form-control"
-                value={this.state.selectColumn}
-                onChange={this.handleChange.bind(this)}
-              >
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>6</option>
-              </select>
-            </div>
-          </div>
-        </div>
-        <div className="row">{listStaff}</div>
-        <div className="row">{this.renderStaff(this.state.selectStaff)}</div>
+      <div key={staff.id} className="col-6 col-md-4 col-lg-2 p-3">
+        <RenderStaff staff={staff} />
       </div>
     );
-  }
+  });
+
+  return (
+    <div className="container p-3">
+      <h3> Nhân Viên</h3>
+      <hr></hr>
+      <div className="row">{staffs}</div>
+    </div>
+  );
 }
 
 export default StaffList;
