b74c1abe1a8cb2a7fdad12390d3d3b645156f643
huongbls <huongntFX16409@funix.edu.vn>
2022-06-01 09:45:26 +0700

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -18,6 +18,7 @@ import {
 import dateFormat from "dateformat";
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from "./LoadingComponent";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -178,7 +179,21 @@ function RenderComment({ comments, addComment, dishId }) {
   }
 }
 const DishDetail = (props) => {
-  if (props.dish != null) {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  } else if (props.errMess) {
+    <div className="container">
+      <div className="row">
+        <h4>{props.errMess}</h4>
+      </div>
+    </div>;
+  } else if (props.dish != null) {
     return (
       <div className="container">
         <div className="row">
