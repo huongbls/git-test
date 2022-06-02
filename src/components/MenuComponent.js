7f1787d8ce7798a190ea68c09c11a6706f08360c
huongbls <huongntFX16409@funix.edu.vn>
2022-06-02 10:23:30 +0700

    Fetch from Server

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -9,12 +9,18 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
 
 function RenderMenuItem({ dish, onClick }) {
   return (
     <Card>
       <Link to={`/menu/${dish.id}`}>
-        <CardImg width="100%" object src={dish.image} alt={dish.name}></CardImg>
+        <CardImg
+          width="100%"
+          object
+          src={baseUrl + dish.image}
+          alt={dish.name}
+        ></CardImg>
 
         <CardImgOverlay>
           <CardTitle>{dish.name}</CardTitle>
